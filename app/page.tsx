55f095073e23385c7fe1c8bc6b18d1fd0d7d8186
smna-alt <sm.na@woowahan.com>
2026-05-13 10:10:51 +0900

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import BaeminEditor from '@/components/BaeminEditor';
+
+export default function Home() {
+  return (
+    <main style={{ height: '100vh', overflow: 'hidden' }}>
+      <BaeminEditor />
+    </main>
+  );
+}
